ba4dc0298ab3ad82a4942fae2675ff7d5d881e07
Kazybekov <kazbekov.ersin@gmail.com>
2022-11-18 03:12:40 +0600

    not easy

--- db.js
+++ db.js
@@ -1,11 +1,20 @@
 const Pool = require("pg").Pool;
-
+require('dotenv').config();
+console.log(process.env.DB_USER)
 const pool = new Pool({
-  user: "postgres",
-  password: "admin",
-  host: "localhost",
-  port: 5432,
-  database: "homework"
+  user: process.env.DB_USER,
+  password:process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME
 });
+
+// const pool = new Pool({
+//   user: "postgres",
+//   password:"admin",
+//   host: "localhost",
+//   port: 5432,
+//   database: "homework"
+// });
 
 module.exports = pool;
