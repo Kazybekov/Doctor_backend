c0a682301a70a3c72883f48a8e722e9b33f31ca0
Kazybekov <kazbekov.ersin@gmail.com>
2022-11-18 04:25:35 +0600

    db fran

--- db.js
+++ db.js
@@ -1,13 +1,23 @@
 const Pool = require("pg").Pool;
-require('dotenv').config();
-console.log(process.env.DB_USER)
+
+require('dotenv').config({
+  path:'../.env'
+})
+
+const constring="postgres://postgres1:dMF9b7xK2Q1o18aC9JN2gJLtHQYAQ7ld@dpg-cdrb535a499bn14rdh8g-a.frankfurt-postgres.render.com/db_assignment"
+
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password:process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME
-});
+  constring,
+})
+// 
+// console.log(process.env.DB_USER)
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   password:process.env.DB_PASSWORD,
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   database: process.env.DB_NAME
+// });
 
 // const pool = new Pool({
 //   user: "postgres",
