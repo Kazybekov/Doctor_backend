99457d2332ef372d2aef13e0b3c611a9e64c4078
Kazybekov <kazbekov.ersin@gmail.com>
2022-11-18 19:16:27 +0600

    connections

--- db.js
+++ db.js
@@ -1,23 +1,23 @@
 const Pool = require("pg").Pool;
 
-// require('dotenv').config({
-//   path:'../.env'
-// })
-//
-// const constring="postgres://postgres1:dMF9b7xK2Q1o18aC9JN2gJLtHQYAQ7ld@dpg-cdrb535a499bn14rdh8g-a.frankfurt-postgres.render.com/db_assignment"
-//
-// const pool = new Pool({
-//   constring,
-// })
+require('dotenv').config({
+  path:'../.env'
+})
+
+const constring="postgres://postgres1:dMF9b7xK2Q1o18aC9JN2gJLtHQYAQ7ld@dpg-cdrb535a499bn14rdh8g-a.frankfurt-postgres.render.com/db_assignment"
+
+const pool = new Pool({
+  constring,
+})
 //
 // console.log(process.env.DB_USER)
-const pool = new Pool({
-  user: process.env.DB_USER,
-  password:process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME
-});
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   password:process.env.DB_PASSWORD,
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   database: process.env.DB_NAME
+// });
 
 // const pool = new Pool({
 //   user: "postgres",
